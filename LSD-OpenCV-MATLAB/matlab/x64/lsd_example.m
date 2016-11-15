2c06719b2d824ff4d4946d61cea6f94459338910
Sebastian Lin <sebastianljs@gmail.com>
2016-11-15 17:52:19 -0500

    Second commit to include Felzenschwalb segmentation code

--- LSD-OpenCV-MATLAB/matlab/x64/lsd_example.m
+++ LSD-OpenCV-MATLAB/matlab/x64/lsd_example.m
@@ -2,11 +2,11 @@
 % Test LSD algorithm with MATLAB
 %% show the image.
 close all; 
-im = imread('./images/Norfolk_01_training.tif');
+im = imread('./images/Norfolk_02_validation.tif');
 imshow(im);
 %% get the start_points and end_points of each straight line use LSD.
 % note: input parameter is the path of image, use '/' as file separator.
-lines = lsd('./images/Norfolk_01_training.tif');
+lines = lsd('./images/Norfolk_02_validation.tif');
 %% plot the lines.
 hold on;
 for i = 1:size(lines, 2)
