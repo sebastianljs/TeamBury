20bbd2bd54f84bf376010ef697f4f395d6b5b6a7
Joyce1114 <yx78@duke.edu>
2016-12-05 10:40:04 -0500

    Add files via upload
    updated

--- roof.m
+++ roof.m
@@ -23,8 +23,8 @@ imshow('cropped.png')
 % imshow(out);
 hold on;
 count = 0;
-verticals = zeros(4,17);
-horizontals = zeros(4,17);
+verticals = zeros(5,17);
+horizontals = zeros(5,17);
 count2 = 0;
 for a = 1:size(edgeim,2)
     if (-2<(edgeim(1,a)-edgeim(2,a))) && ((edgeim(1,a)-edgeim(2,a))<2)
@@ -48,20 +48,37 @@ for a = 1:size(edgeim,2)
 end
 
 
-
-figure(4)
+hypo = zeros(5,1088);
+count3 = 0;
 for i = 1:size(verticals,2)-1
     for j = i+1:size(verticals,2)
         if abs(verticals(1,i) - verticals(1,j))<40 && abs(verticals(1,i) - verticals(1,j))>10 && abs(verticals(3,i)-verticals(3,j))<20
            for a=1:size(horizontals,2)-1
                for b = a+1:size(horizontals,2)
-                   if abs(horizontals(1,a) - verticals(1,i))<10 && abs(horizontals(1,b)-verticals(1,j)<10)
-                       imshow('cropped.png')
-                       hold on;
-                       plot(verticals(1:2, i), verticals(3:4, i), 'LineWidth', verticals(5, i) / 2, 'Color', [1, 0, 0]);
-                        plot(verticals(1:2, j), verticals(3:4, j), 'LineWidth', verticals(5, j) / 2, 'Color', [1, 0, 0]);
-                         plot(horizontals(1:2, a), horizontals(3:4, a), 'LineWidth', horizontals(5, a) / 2, 'Color', [1, 0, 0]);
-                          plot(horizontals(1:2, b), horizontals(3:4, b), 'LineWidth', horizontals(5, b) / 2, 'Color', [1, 0, 0]);
+                   if abs(horizontals(3,a) - horizontals(3,b))<40 && abs(horizontals(3,a) - horizontals(3,b))>10 && abs(horizontals(1,a)-horizontals(1,b))<30
+                      hypo(1,4*count3+1) = verticals(1,i);
+                      hypo(2,4*count3+1) = verticals(2,i);
+                      hypo(3,4*count3+1) = verticals(3,i);
+                      hypo(4,4*count3+1) = verticals(4,i);
+                      hypo(5,4*count3+1) = verticals(5,i);
+                      hypo(1,4*count3+2) = verticals(1,j);
+                      hypo(2,4*count3+2) = verticals(2,j);
+                      hypo(3,4*count3+2) = verticals(3,j);
+                      hypo(4,4*count3+2) = verticals(4,j);
+                      hypo(5,4*count3+2) = verticals(5,j);
+                       hypo(1,4*count3+3) = horizontals(1,a);
+                      hypo(2,4*count3+3) = horizontals(2,a);
+                      hypo(3,4*count3+3) = horizontals(3,a);
+                      hypo(4,4*count3+3) = horizontals(4,a);
+                      hypo(5,4*count3+3) = horizontals(5,a);
+                       hypo(1,4*count3+4) = horizontals(1,b);
+                      hypo(2,4*count3+4) = horizontals(2,b);
+                      hypo(3,4*count3+4) = horizontals(3,b);
+                      hypo(4,4*count3+4) = horizontals(4,b);
+                      hypo(5,4*count3+4) = horizontals(5,b);
+                      count3 = count3+1;
+                      
+                      
                    end
                end
            end
